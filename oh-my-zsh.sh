a61d4f67a1d7726aa5d75ae2604fd08d23083bc2
kerron <kerron68@gmail.com>
2020-10-05 13:45:30 +0100

    new vimrc

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -89,17 +89,8 @@ alias szsh='source ~/.zshrc'
 alias gitrc='vim ~/.oh-my-zsh/plugins/git/git.plugin.zsh'
 alias zshrc='vim ~/.zshrc'
 alias vimrc='vim ~/.vimrc'
-alias python='python3'
-alias rpe='cd /Users/kerron/Downloads/reinfer/ && source env/bin/activate && cd /Users/kerron/Downloads/reinfer/platform && j webapp' 
 alias f='flutter'
-alias pd='cd ~/Downloads/reinfer/platform && ./ci/jenkins/fs/scripts/ci-purge-docker' 
-alias ssp='cd ~/Downloads/reinfer/platform && ./tools/start-dev-environment --pull' 
-alias ssf='cd ~/Downloads/reinfer/platform && ./tools/start-dev-environment' 
-alias ss='cd ~/Downloads/reinfer/platform && ./tools/start-dev-environment --minimal' 
 alias rna='react-native run-android'
-alias ys='yarn start'
-alias yi='yarn install --force'
-alias yb='yarn build'
 alias gvrc='vi ~/.oh-my-zsh/plugins/git/git.plugin.zsh'
 alias yt='yarn test --testPathPattern '
 alias ytw='yarn test --testPathPattern --watch '
@@ -123,101 +114,9 @@ prompt_dir() {
 function cd {
   builtin cd "$@" && ls
 }
-export FZF_DEFAULT_COMMAND='fd --type f'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
 export PATH="$PATH:/Users/kerron/Downloads/dev/flutter/bin"
-# The ip address that docker containers will bind to.
-export DOCKER_GATEWAY="192.168.65.2"
-export DOCKER_COMPOSE_BIND="127.0.0.1"
 
-# export DOCKER_COMPOSE_BIND="$(docker network inspect bridge -f '{{(index .IPAM.Config 0).Gateway}}')"
-# if [ `uname` = "Darwin" ]; then
-  # export DOCKER_GATEWAY="host.docker.internal"
-# else
-  # export DOCKER_GATEWAY=$DOCKER_COMPOSE_BIND
-# fi
-# export DOCKER_GATEWAY="172.17.0.1"
-# The rethinkdb endpoint for tests.
-export RETHINKDB_TEST_ENDPOINT=localhost:28015
-
-# The ldap endpoint for tests.
-export LDAP_TEST_ENDPOINT=localhost:8389
-
-# The elasticsearch endpoint for tests.
-export ELASTICSEARCH_TEST_ENDPOINT=localhost:9200
-export DUCKLING_TEST_ENDPOINT=localhost:8026
-
-# The prefix used for rethinkdb test databases (you'll need a different one if
-# running multiple instances of `py.test` at once.
-export REINFER_TEST_NAMESPACE='python'
-
-# The default model family for datasets.
-# For a real deployment this would typically be `english`. `english-dev` is the
-# same model architecture, but trained only with very few iteratioons. An
-# alternative fast model is `random`, but expect random predictions.
-export DEFAULT_MODEL_FAMILY="english-dev"
-
-# The model for unsupervised learning (bulk label / discover page). Change to
-# `random-k30` for fast (but random) model for development.
-export THEME_MODEL_ID="ne-rs-cnmf-max4096-k30"
-
-# docker-compose project name
-export COMPOSE_PROJECT_NAME='reinfer'
-
-# Diff runner defaults
-export DIFF_RUNNER_DOCKER_NETWORK='reinfer_default'
-export DIFF_RUNNER_LINKS='RETHINKDB_TEST_ENDPOINT=rethinkdb:reinfer_rethinkdb_1:28015,ELASTICSEARCH_TEST_ENDPOINT=elasticsearch:reinfer_elasticsearch_1:9200,DUCKLING_TEST_ENDPOINT=duckling:reinfer_duckling_1:8026,LDAP_TEST_ENDPOINT=openldap:reinfer_openldap_1:8389'
-# The prefix used for rethinkdb test databases (you'll need a different one if
-# running multiple instances of `py.test` at once.
-
-export OMP_NUM_THREADS=4
-
-export MYPYPATH=${HOME}/Downloads/reinfer/platform/api:${HOME}/Downloads/reinfer/platform/auth:${HOME}/Downloads/reinfer/platform/index:${HOME}/Downloads/reinfer/platform/user-model:${HOME}/Downloads/reinfer/platform/user-model-store:${HOME}/Downloads/reinfer/platform/predict/server:${HOME}/Downloads/reinfer/platform/predict/shared:${HOME}/Downloads/reinfer/platform/pycommon:${HOME}/Downloads/reinfer/platform/pyrethool:${HOME}/Downloads/reinfer/platform/pyweb:${HOME}/Downloads/reinfer/platform/refile/client:${HOME}/Downloads/reinfer/platform/refile/server:${HOME}/Downloads/reinfer/platform/rpz:${HOME}/Downloads/reinfer/platform/sentence:${HOME}/Downloads/reinfer/platform/store:${HOME}/Downloads/reinfer/platform/summary/client:${HOME}/Downloads/reinfer/platform/summary/server:${HOME}/Downloads/reinfer/platform/theme:${HOME}/Downloads/reinfer/platform/train:${HOME}/Downloads/reinfer/platform/trigger/server:${HOME}/Downloads/reinfer/platform/trigger/client:${HOME}/Downloads/reinfer/platform/labels-gc:${HOME}/Downloads/reinfer/platform/entity/client:${HOME}/Downloads/reinfer/platform/entity/server:${HOME}/Downloads/reinfer/platform/duckling/client:${HOME}/Downloads/reinfer/platform/sources-gc
-
- #Tunnel to jenkins.
-function jenkins () {
-    local port1=${1:-39101}
-    local port2=${2:-39102}
-    sh -c "sleep 2 && open http://localhost:${port1}" &
-    gcloud compute ssh --project=reinfer-ci --zone=europe-west1-c jenkins -- -L ${port1}:localhost:8080 -L ${port2}:localhost:8971
-}
-
-# Tunnel to staging rethinkdb database.
-function staging-rethink () {
-    local port=${1:-38016}
-    gcloud compute ssh --project=reinfer-staging --zone=europe-west1-c rethink-1 -- -L ${port}:rethink-1:28015 -L 30048:rethink-1:8080
-}
-
-# Tunnel to prod rethinkdbdatabase.
-function prod-rethink () {
-    local port=${1:-48016}
-    gcloud compute ssh --project=reinfer-prod --zone=europe-west1-d rethink-7k30 -- -L ${port}:rethink-7k30:28015 -L 40048:rethink-7k30:8080
-}
-
-
-# Staging kubectl shortcut.
-alias kcs="kubectl --cluster gke_reinfer-staging_europe-west1-c_main"
-# Prod kubectl shortcut.
-alias kcp="kubectl --cluster gke_reinfer-prod_europe-west1-c_main"
-
-# Docker compose shortcuts.
-alias dc='docker-compose'
-alias dcr='docker-compose rm'
-alias dcu='docker-compose up'
-alias dcs='docker-compose stop'
-alias dcb='docker-compose build'
-
-# Gcloud shortcuts
-alias cl=gcloud
-alias co='gcloud compute'
-alias cssh='gcloud compute ssh'
-alias gu=gsutil
-alias start_services='~/Downloads/reinfer/platform/tools/start-dev-environment'
-alias rcon='/Users/kerron/Downloads/reinfer/platform/tools/gcp/rcon'
-
-# SOURCE
-source <(kubectl completion zsh)
-source '/Users/kerron/google-cloud-sdk/completion.zsh.inc'
 
 # export MANPATH="/usr/local/man:$MANPATH"
 
@@ -245,15 +144,6 @@ source '/Users/kerron/google-cloud-sdk/completion.zsh.inc'
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
-wd() {
-  . /Users/kerron/bin/wd/wd.sh
-}
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/kerron/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/kerron/google-cloud-sdk/path.zsh.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '/Users/kerron/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/kerron/google-cloud-sdk/completion.zsh.inc'; fi
 
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
@@ -261,5 +151,3 @@ source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 export ZPLUG_HOME=/usr/local/opt/zplug
 source $ZPLUG_HOME/init.zsh
 source /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh
-
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
