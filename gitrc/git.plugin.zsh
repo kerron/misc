cf65aad8283bf79bce4364861792377286e370af
Kerron <kerron68@gmail.com>
2021-02-01 10:42:30 +0000

    git update

---
+++ gitrc/git.plugin.zsh
@@ -0,0 +1,10 @@
+# CUSTOM
+alias ga='git add .'
+alias gc='git commit -m'
+alias gcom='git checkout master'
+alias gpl='git pull'
+alias gpsu='git push --set-upstream origin $(git_current_branch)'
+alias gs='git status'
+alias gcob='git checkout -b'
+
+
